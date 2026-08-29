d56378acfa2a3f7a427975c7f8418387bc8871a9
joseninogarcia06 <jngarcia08081988@gmail.com>
2026-08-29 11:29:22 +0800

    Splash Screen Added
    Implementation of the Splash Screen

--- findmyapartment/app/_layout.tsx
+++ findmyapartment/app/_layout.tsx
@@ -1,5 +1,28 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+
+  });
+
+  useEffect(() => {
+    if (fontError){
+      console.error("Font loading error:", fontError);
+    }
+    
+    if(fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if(!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return <Stack />;
 }
--- findmyapartment/app/index.tsx
+++ findmyapartment/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Splash screen not working</Text>
     </View>
   );
 }
